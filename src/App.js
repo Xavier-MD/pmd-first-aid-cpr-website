191fa07471586d57592d7cc4847b4838a43a7bdc
Xavier-MD <xavier.pmd@outlook.com>
2022-02-23 20:52:46 -0500

    moved navbar and footer into folders

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Navbar from './components/common/Navbar';
+import Navbar from './components/common/Navbar/Navbar';
 import Landing from './pages/Landing';
 import About from './pages/About';
 import Courses from './pages/Courses';
 import Testimonials from './pages/Testimonials';
 import Contact from './pages/Contact';
 import Error from './pages/Error';
-import Footer from './components/common/Footer';
+import Footer from './components/common/Footer/Footer';
 
 function App() {
   return (
