a595f142948f18dd46d419568a18d73008d0b408
Xavier-MD <xavier.pmd@outlook.com>
2022-03-01 08:40:36 -0500

    created a navbar component

--- src/components/common/Navbar/Navbar.jsx
+++ src/components/common/Navbar/Navbar.jsx
@@ -1,10 +1,117 @@
-import React from 'react';
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 function Navbar() {
   return (
-    <div className='Navbar'>
-      <h2>Navbar</h2>
-    </div>
+    <Disclosure as='nav' className='bg-white shadow'>
+      {({ open }) => (
+        <>
+          <div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8'>
+            <div className='relative flex justify-between h-16'>
+              <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
+                {/* Mobile menu button */}
+                <Disclosure.Button className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
+                  <span className='sr-only'>Open main menu</span>
+                  {open ? (
+                    <XIcon className='block h-6 w-6' aria-hidden='true' />
+                  ) : (
+                    <MenuIcon className='block h-6 w-6' aria-hidden='true' />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className='flex-1 flex items-center justify-center sm:items-stretch sm:justify-start'>
+                <div className='flex-shrink-0 flex items-center'>
+                  <img
+                    className='block lg:hidden h-8 w-auto'
+                    src='https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg'
+                    alt='Workflow'
+                  />
+                  <img
+                    className='hidden lg:block h-8 w-auto'
+                    src='https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg'
+                    alt='Workflow'
+                  />
+                </div>
+                <div className='hidden sm:ml-6 sm:flex sm:space-x-8'>
+                  {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
+                  <a
+                    href='/'
+                    className='border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+                  >
+                    Home
+                  </a>
+                  <a
+                    href='/about-us'
+                    className='border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+                  >
+                    About Us
+                  </a>
+                  <a
+                    href='/courses'
+                    className='border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+                  >
+                    Courses
+                  </a>
+                  <a
+                    href='/testimonials'
+                    className='border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+                  >
+                    Testimonials
+                  </a>
+                </div>
+              </div>
+              <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
+                <a
+                  href='/contact-us'
+                  className='border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+                >
+                  Contact Us
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className='sm:hidden'>
+            <div className='pt-2 pb-4 space-y-1'>
+              {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
+              <Disclosure.Button
+                as='a'
+                href='#'
+                className='bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+              >
+                Dashboard
+              </Disclosure.Button>
+              <Disclosure.Button
+                as='a'
+                href='#'
+                className='border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+              >
+                Team
+              </Disclosure.Button>
+              <Disclosure.Button
+                as='a'
+                href='#'
+                className='border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+              >
+                Projects
+              </Disclosure.Button>
+              <Disclosure.Button
+                as='a'
+                href='#'
+                className='border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+              >
+                Calendar
+              </Disclosure.Button>
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   );
 }
 
