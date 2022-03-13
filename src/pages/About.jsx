4401ee79045414bc69bfb8a60e08040ca88c9021
Xavier-MD <xavier.pmd@outlook.com>
2022-03-13 19:10:19 -0400

    added an about us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,121 @@
-import React from 'react';
-
-function About() {
+const About = function () {
   return (
-    <div className='About'>
-      <h2>About</h2>
+    <div className='bg-white overflow-hidden'>
+      <div className='relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8'>
+        <div className='hidden lg:block bg-gray-50 absolute top-0 bottom-0 right-[120%] w-screen' />
+        <div className='mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none'>
+          <div>
+            <h3 className='mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
+              Meet Paul
+            </h3>
+          </div>
+        </div>
+        <div className='mt-8 lg:grid lg:grid-cols-2 lg:gap-8'>
+          <div className='relative lg:row-start-1 lg:col-start-2'>
+            <svg
+              className='hidden lg:block absolute top-0 right-0 -mt-20 -mr-20'
+              width={404}
+              height={384}
+              fill='none'
+              viewBox='0 0 404 384'
+              aria-hidden='true'
+            >
+              <defs>
+                <pattern
+                  id='de316486-4a29-4312-bdfc-fbce2132a2c1'
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits='userSpaceOnUse'
+                >
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className='text-gray-200'
+                    fill='currentColor'
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={404}
+                height={384}
+                fill='url(#de316486-4a29-4312-bdfc-fbce2132a2c1)'
+              />
+            </svg>
+            <div className='relative text-base mx-auto max-w-prose lg:max-w-none'>
+              <figure>
+                <div className='aspect-none'>
+                  <img
+                    className=''
+                    src='/images/paul-picture.png'
+                    alt=''
+                    width={1184}
+                    height={1376}
+                  />
+                </div>
+              </figure>
+            </div>
+          </div>
+          <div className='mt-8 lg:mt-0'>
+            <div className='text-base max-w-prose mx-auto lg:max-w-none'>
+              <p className='text-lg text-gray-500'>
+                Sagittis scelerisque nulla cursus in enim consectetur quam.
+                Dictum urna sed consectetur neque tristique pellentesque.
+                Blandit amet, sed aenean erat arcu morbi.
+              </p>
+            </div>
+            <div className='mt-5 prose prose-indigo text-gray-500 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1'>
+              <p>
+                Sollicitudin tristique eros erat odio sed vitae, consequat
+                turpis elementum. Lorem nibh vel, eget pretium arcu vitae. Eros
+                eu viverra donec ut volutpat donec laoreet quam urna.
+              </p>
+              <p>
+                Bibendum eu nulla feugiat justo, elit adipiscing. Ut tristique
+                sit nisi lorem pulvinar. Urna, laoreet fusce nibh leo. Dictum et
+                et et sit. Faucibus sed non gravida lectus dignissim imperdiet
+                a.
+              </p>
+              <p>
+                Dictum magnis risus phasellus vitae quam morbi. Quis lorem lorem
+                arcu, metus, egestas netus cursus. In.
+              </p>
+              <h3>Our mission Statement</h3>
+              <ul>
+                <li>Quis elit egestas venenatis mattis dignissim.</li>
+                <li>
+                  Cras cras lobortis vitae vivamus ultricies facilisis tempus.
+                </li>
+                <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
+              </ul>
+              <p>
+                Rhoncus nisl, libero egestas diam fermentum dui. At quis
+                tincidunt vel ultricies. Vulputate aliquet velit faucibus
+                semper. Pellentesque in venenatis vestibulum consectetur nibh
+                id. In id ut tempus egestas. Enim sit aliquam nec, a. Morbi enim
+                fermentum lacus in. Viverra.
+              </p>
+              <p>
+                Tincidunt integer commodo, cursus etiam aliquam neque, et.
+                Consectetur pretium in volutpat, diam. Montes, magna cursus
+                nulla feugiat dignissim id lobortis amet. Laoreet sem est
+                phasellus eu proin massa, lectus. Diam rutrum posuere donec
+                ultricies non morbi. Mi a platea auctor mi.
+              </p>
+              <p>
+                Sagittis scelerisque nulla cursus in enim consectetur quam.
+                Dictum urna sed consectetur neque tristique pellentesque.
+                Blandit amet, sed aenean erat arcu morbi.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default About;
