5422f5205be295c82d040f419337efc93242105c
Xavier-MD <xavier.pmd@outlook.com>
2022-03-28 10:45:55 -0400

    edited course page header

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -3,7 +3,7 @@ import ButtonPane from '../components/Courses/ButtonPane';
 function Courses({ course, setCourse }) {
   return (
     <div className='flex flex-col items-center pt-10'>
-      <p className='text-slate-50 font-heading font-semibold text-[3em] border-4 border-slate-200 rounded-full px-5 bg-indigo-500'>Our Courses</p>
+      <p className='text-slate-700 font-header font-bold text-[3em] border-b-2 border-slate-200 px-5'>Courses</p>
       <ButtonPane course={course} setCourse={setCourse} />
     </div>
   );
