3674c1cf7ce91ab895329d198e0aeac28e27b3af
Xavier-MD <xavier.pmd@outlook.com>
2022-03-28 12:41:13 -0400

    enlarged the size of current tabs

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -3,7 +3,6 @@ import ButtonPane from '../components/Courses/ButtonPane';
 function Courses({ course, setCourse }) {
   return (
     <div className='flex flex-col items-center pt-10'>
-      <p className='text-slate-700 font-header font-bold text-[3em] border-b-2 border-slate-200 px-5'>Courses</p>
       <ButtonPane course={course} setCourse={setCourse} />
     </div>
   );
