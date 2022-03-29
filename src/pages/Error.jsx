df6c4df9eaff15ed6e7f8e4aa3e64c9cb0c25598
Xavier-MD <xavier.pmd@outlook.com>
2022-03-29 18:41:56 -0400

    changed the error page's colors

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -6,7 +6,7 @@ const Error = function () {
       <div className='bg-white min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8'>
         <div className='max-w-max mx-auto'>
           <main className='sm:flex'>
-            <p className='text-4xl font-extrabold text-indigo-600 sm:text-5xl'>
+            <p className='text-4xl font-extrabold text-blue-600 sm:text-5xl'>
               404
             </p>
             <div className='sm:ml-6'>
@@ -21,13 +21,13 @@ const Error = function () {
               <div className='mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6'>
                 <a
                   href='/'
-                  className='inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                  className='inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500'
                 >
                   Go back home
                 </a>
                 <a
                   href='/contact-us'
-                  className='inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                  className='inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-blue-700 bg-blue-200 hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500'
                 >
                   Contact Us
                 </a>
