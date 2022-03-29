82bc014ef16126a59bbacd5080f4a0fa87671496
Xavier-MD <xavier.pmd@outlook.com>
2022-03-29 18:02:43 -0400

    created a new url for individual course pages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useParams } from 'react-router-dom';
 
 import Navbar from './pages/Navbar';
 import Landing from './pages/Landing';
 import About from './pages/About';
-import Courses from './pages/Courses';
+import AllCourses from './pages/AllCourses';
+import CourseInfo from './components/Courses/CourseInfo';
 import Testimonials from './pages/Testimonials';
 import Contact from './pages/Contact';
 import Error from './pages/Error';
 import Footer from './pages/Footer';
 
 function App() {
-  const [course, setCourse] = useState('Standard First Aid');
+  const { id } = useParams();
   return (
-    <div className='App'>
+    <div className='min-h-screen flex flex-col justify-between'>
       <Navbar />
       <Routes>
         <Route path='/' element={<Landing />} />
         <Route path='/about-us' element={<About />} />
-        <Route
-          path='/courses'
-          element={<Courses course={course} setCourse={setCourse} />}
-        />
+        <Route path='/courses' element={<AllCourses />} />
+        <Route path='/courses/:id' element={<CourseInfo course={id} />} />
         <Route path='/testimonials' element={<Testimonials />} />
         <Route path='/contact-us' element={<Contact />} />
         <Route path='*' element={<Error />} />
