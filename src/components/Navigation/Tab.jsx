2be3e80989789ee3a74d46f482d7726ec9b46107
Xavier-MD <xavier.pmd@outlook.com>
2022-04-10 18:59:06 -0400

    changed the animation of all standard tags

--- src/components/Navigation/Tab.jsx
+++ src/components/Navigation/Tab.jsx
@@ -1,17 +1,37 @@
-import classNames from "classnames";
-  
-const Tab = function({ tabInfo }) {
+import classNames from 'classnames';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDiamond } from '@fortawesome/free-solid-svg-icons';
+
+const Tab = function({ tabInfo, moveLeft }) {
   return (
     <a
       href={tabInfo.href}
       className={classNames(
-        tabInfo.href === window.location.pathname
-          ? 'border-blue-400 text-gray-900 font-semibold cursor-default'
-          : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-200 delay-25',
-        'px-4 inline-flex font-header items-center border-b-[2px] tracking-tighter text-xl'
+        window.location.pathname === tabInfo.href ? 'cursor-default' : '',
+        'group mt-2 inline-flex flex-col font-header justify-center items-center focus:outline-none'
       )}
     >
-      {tabInfo.name}
+      <div className='flex flex-row items-center'>
+        <FontAwesomeIcon
+          icon={faDiamond}
+          className={classNames(
+            window.location.pathname === tabInfo.href ? '' : 'hidden',
+            `w-2 mr-[8px] text-slate-800 animate-pulse transition ease-in-out group-hover:-translate-y-[1px] group-hover:-translate-x-[10px] group-hover:scale-[125%] duration-500`
+          )}
+          aria-hidden='true'
+        />
+        <h2
+          className={classNames(
+            window.location.pathname === tabInfo.href
+              ? 'text-slate-900'
+              : 'text-slate-600 hover:text-slate-900',
+            'text-lg transition ease-in-out group-hover:-translate-y-[2px] group-hover:scale-[125%] duration-500'
+          )}
+        >
+          {tabInfo.name}
+        </h2>
+      </div>
+      <div className='w-1/4 opacity-0 h-[1px] bg-slate-800 transition ease-in-out group-hover:opacity-100 group-hover:scale-x-[600%] duration-300' />
     </a>
   );
 };
