31ffdb471812ef4ffa1c5a422198702ac9891c52
Xavier-MD <xavier.pmd@outlook.com>
2022-04-11 00:24:39 -0400

    changed to use active state instead of focus

--- src/components/Navigation/TabIcon.jsx
+++ src/components/Navigation/TabIcon.jsx
@@ -1,12 +1,17 @@
-import { MailIcon } from "@heroicons/react/outline";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 const TabIcon = function ({ tabInfo }) {
   return (
     <a
       href={tabInfo.href}
-      className='inline-flex bg-blue-500 py-2 px-4 rounded text-lg text-blue-50 shadow-md shadow-blue-400 hover:text-slate-300 hover:bg-blue-600 active:outline-none active:ring-2 active:ring-slate-300 active:bg-blue-700'
+      className='w-14 h-10 inline-flex justify-center items-center bg-blue-500  rounded text-lg text-blue-50 shadow-md shadow-blue-400 active:bg-blue-600 focus:outline-none active:ring-2 active:ring-slate-300'
     >
-      <MailIcon className='block h-6 w-6' aria-hidden='true' />
+      <FontAwesomeIcon
+        icon={faEnvelope}
+        className='w-6 h-6'
+        aria-hidden='true'
+      />
     </a>
   );
 };
