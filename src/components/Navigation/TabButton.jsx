41811dab45d9bdfe7c5cdf706c4b40641afa4606
Xavier-MD <xavier.pmd@outlook.com>
2022-04-11 00:24:06 -0400

    changed tab button's color when on contact page

--- src/components/Navigation/TabButton.jsx
+++ src/components/Navigation/TabButton.jsx
@@ -6,7 +6,7 @@ const TabButton = function({ tabInfo }) {
       href={tabInfo.href}
       className={classNames(
         window.location.pathname === tabInfo.href
-          ? 'bg-gradient-to-br from-green-500 to-lime-300 text-slate-700 shadow-lime-600 hover:bg-gradient-to-tl active:bg-gradient-to-bl'
+          ? 'bg-gradient-to-br from-green-500 to-lime-400 text-slate-50 shadow-lime-600 hover:bg-gradient-to-tl active:bg-gradient-to-bl'
           : 'bg-blue-500 text-slate-50 shadow-blue-400 hover:bg-blue-600',
         'inline-flex py-2 px-4 rounded font-header text-lg shadow-md active:bg-blue-700 transition ease-in-out hover:-translate-y-[1px] hover:scale-110 duration-200'
       )}
