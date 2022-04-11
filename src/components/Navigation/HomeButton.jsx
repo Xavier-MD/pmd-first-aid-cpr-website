80a29b9c0d5f694c788f0a1db57685d3cd8b1300
Xavier-MD <xavier.pmd@outlook.com>
2022-04-11 00:22:40 -0400

    closed the bottom of the home button's circular border and created a course tab selector

--- src/components/Navigation/HomeButton.jsx
+++ src/components/Navigation/HomeButton.jsx
@@ -2,7 +2,7 @@ const HomeButton = function () {
   return (
     <a href='/'>
       <img
-        className='w-24 h-auto rotate-180'
+        className='w-24 h-auto'
         src='/images/logo-small-text.png'
         alt='PMD Logo'
       />
--- src/components/Navigation/NavigationBar.jsx
+++ src/components/Navigation/NavigationBar.jsx
@@ -17,24 +17,30 @@ const tabsInfo = {
 
 const NavigationBar = function () {
   const [open, setOpen] = useState(false);
+  let currentCourseTab = {};
+  if (window.location.pathname === '/courses') {
+    currentCourseTab = <Tab tabInfo={tabsInfo.courses} moveLeft='10' />;
+  } else {
+    currentCourseTab = <CoursesDropdown moveLeft='10' />;
+  }
 
   return (
-    <nav className='w-full h-24 fixed z-20'>
-      <div className='px-8 xl:px-20 bg-gradient-to-b from-blue-400 via-blue-200 to-white'>
+    <nav className='w-full h-auto fixed z-20'>
+      <div className='h-[85px] px-8 xl:px-20 bg-gradient-to-b from-blue-400 via-blue-200 to-blue-50 shadow-md shadow-slate-300'>
         {/* Mobile menu button */}
         <div className='absolute inset-y-0 left-0 pl-6 flex items-center lg:hidden'>
           <MobileDropdown open={open} setOpen={setOpen} tabsInfo={tabsInfo} />
         </div>
         {/* Desktop nav flexbox */}
-        <div className='w-full flex justify-center lg:justify-between'>
+        <div className='w-full h-full flex justify-center lg:justify-between'>
           <div className='hidden max-w-xl lg:flex flex-1 justify-around items-center'>
             <Tab tabInfo={tabsInfo.homepage} moveLeft='8' />
             <Tab tabInfo={tabsInfo.aboutUs} moveLeft='12' />
-            <CoursesDropdown moveLeft='10' />
+            {currentCourseTab}
             <Tab tabInfo={tabsInfo.testimonials} moveLeft='16' />
           </div>
-          <div className='mt-2 -mb-8 flex justify-center'>
-            <div className='mx-2 p-3 border-b-[3px] border-slate-300 rounded-full rotate-180 shadow-lg shadow-blue-400'>
+          <div className='mt-2 -mb-[47px] flex justify-center'>
+            <div className='mx-2 p-3 bg-gradient-to-b from-blue-300 to-white border-[3px] border-slate-200 rounded-full shadow-md shadow-slate-300'>
               <HomeButton />
             </div>
           </div>
@@ -46,9 +52,6 @@ const NavigationBar = function () {
         <div className='absolute inset-y-0 right-0 pr-6 flex items-center lg:hidden'>
           <TabIcon tabInfo={tabsInfo.contactUs} />
         </div>
-      </div>
-      <div className='w-full border-b-transparent flex justify-center'>
-        <span className='block w-1/2 h-[2px] bg-gradient-to-r from-transparent via-blue-400 to-transparent'></span>
       </div>
     </nav>
   );
