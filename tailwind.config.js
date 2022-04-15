f6df8327b7234b1683b19c01319d74e92c86caff
Xavier-MD <xavier.pmd@outlook.com>
2022-04-15 00:15:14 -0400

    decided on final fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/flowbite/**/*.js'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     fontFamily: {
       sans: ['Inter var'],
+      thick: ['Work Sans'],
       header: ['Source Sans Pro'],
-      body: ['Roboto'],
-      bodyAlt:['Work Sans']
+      body: ['Roboto']
     }
   },
   plugins: [
