cf1bc9a65e33bebcc8092ca0aa632a1a7fd25e6c
Xavier-MD <xavier.pmd@outlook.com>
2022-04-19 11:33:02 -0400

    removed an unused code block

--- src/components/Testimonials/TestimonialCard.jsx
+++ src/components/Testimonials/TestimonialCard.jsx
@@ -59,19 +59,3 @@ const TestimonialCard = function() {
   );
 }
 export default TestimonialCard;
-
-{
-  /* <div className='flex items-center justify-center text-9xl h-64 w-64 rounded-full xl:h-80 xl:w-80 border-4 border-slate-900 border-opacity-50 shadow-2xl shadow-slate-500'>
-  <div className='flex'>
-    <p className='pl-10 text-9xl text-slate-900 text-opacity-90'>5</p>
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      className='h-24 w-24 text-slate-900 opacity-90'
-      viewBox='0 0 24 24'
-      fill='currentColor'
-    >
-      <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-    </svg>
-  </div>
-</div>; */
-}
