0b91c85c7e17d15a3e58bab341042ab775bd0cd8
Xavier-MD <xavier.pmd@outlook.com>
2022-04-19 11:31:04 -0400

    altered course button design

--- src/components/Courses/CourseButton.jsx
+++ src/components/Courses/CourseButton.jsx
@@ -2,7 +2,7 @@ const CourseButton = function ({ course }) {
   return (
     <a
       href={course.href}
-      className='inline-flex items-center py-8 border border-transparent text-xl tracking-tighter font-medium rounded-md text-slate-700 bg-blue-200 hover:bg-blue-300 w-[90%] justify-center font-header shadow-lg shadow-blue-300'
+      className='uppercase inline-flex font-thick text-center items-center py-6 text-lg rounded-2xl text-slate-700 bg-slate-200 hover:bg-blue-200 hover:font-semibold border-4 border-blue-300 w-[90%] justify-center shadow-md shadow-blue-400 tracking-wide transition ease-in-out duration-100 hover:scale-110 hover:border-blue-400'
     >
       {course.name}
     </a>
