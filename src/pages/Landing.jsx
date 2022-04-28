5baef857caa4127513e1bc95ed6e19438c1f87bf
Xavier-MD <xavier.pmd@outlook.com>
2022-04-28 17:52:58 -0400

    Redesigned landing page and deployed on firebase

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,207 +1,212 @@
-import NavigationBar from '../components/Navigation/NavigationBar';
-import Footer from '../components/Navigation/Footer';
-import {
-  ExclamationCircleIcon,
-  ExclamationIcon,
-  HeartIcon,
-  SupportIcon
-} from '@heroicons/react/outline';
-
-const features = [
+const courses = [
   {
     name: 'Standard First Aid',
+    href: '/courses/standard-first-aid',
+    imageSrc: '/images/standard-first-aid-picture.jpg',
+    imageAlt: 'SFA Picture',
     description:
-      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: ExclamationCircleIcon
+      'A comprehensive two-day course offering first aid and cardiopulmonary resuscitation (CPR) skills for those who need training due to work requirements or who want more knowledge to respond to emergencies at home...'
   },
   {
     name: 'Emergency First Aid',
+    href: '/courses/emergency-first-aid',
+    imageSrc: '/images/emergency-first-aid-picture.jpg',
+    imageAlt: 'EFA Picture',
     description:
-      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: ExclamationIcon
+      'A basic one-day course offering lifesaving first aid and cardiopulmonary resuscitation (CPR) skills for the workplace or home...'
   },
   {
     name: 'CPR & AED',
+    href: '/courses/cpr-aed',
+    imageSrc: '/images/cpr-aed-picture.jpg',
+    imageAlt: 'CPR AED Picture',
     description:
-      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: HeartIcon
+      'Courses on cardiopulmonary resuscitation (CPR) provide the skills needed to recognize and respond to cardiovascular emergencies and choking for adults, children, and babies depending on the level of CPR chosen...'
   },
   {
     name: 'Basic Life Support',
+    href: '/courses/basic-life-support',
+    imageSrc: '/images/bls-picture.jpg',
+    imageAlt: 'BLS Picture',
     description:
-      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: SupportIcon
-  }
-];
-const metrics = [
-  {
-    id: 1,
-    stat: '10,000 +',
-    emphasis: 'People',
-    rest: 'have successfully received certifications through PMD.'
-  },
-  {
-    id: 2,
-    stat: '20',
-    emphasis: 'Years',
-    rest: 'of CPR training experience.'
-  },
-  {
-    id: 3,
-    stat: '98%',
-    emphasis: 'Customer satisfaction',
-    rest: 'rates with shining reviews visible on our testimonials page.'
-  },
-  {
-    id: 4,
-    stat: '2',
-    emphasis: 'Languages',
-    rest: 'for a fully bilingual and immersive training experience.'
+      'Designed to build participant confidence in performing cardiopulmonary resuscitation (CPR) skills in a team environment for professionals with a duty to respond...'
   }
 ];
 
 const Landing = function () {
   return (
-    <>
-      <div className='bg-white mt-44'>
-        <main>
-          {/* Hero section */}
-          <div className='relative'>
-            <div className='absolute inset-x-0 bottom-0 h-1/2 bg-gray-100' />
-            <div className='max-w-7xl mx-auto sm:px-6 lg:px-8'>
-              <div className='relative shadow-xl sm:rounded-2xl sm:overflow-hidden'>
-                <div className='absolute inset-0'>
-                  <img
-                    className='h-full w-full object-cover'
-                    src='/images/group-cpr-picture.jpg'
-                    alt='People taking a cpr course'
-                  />
-                  <div className='absolute inset-0 bg-gradient-to-r from-blue-800 to-blue-200 mix-blend-multiply' />
-                </div>
-                <div className='relative px-4 py-16 sm:px-6 sm:py-24 lg:pt-16 lg:px-8 flex flex-col items-center'>
-                  <img
-                    className='h-40 w-32'
-                    src='/images/logo-no-text.png'
-                    alt=''
-                  />
-                  <h1 className='text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl'>
-                    <span className='block text-white'>
-                      PMD First Aid & CPR
-                    </span>
-                    <span className='block text-blue-200 text-[50%] font-light mt-2'>
-                      Bilingual & Equity Driven
-                    </span>
-                  </h1>
-                  <div className='mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center'>
-                    <a
-                      href='/about-us'
-                      className='flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-blue-500 bg-opacity-60 hover:bg-opacity-70 sm:px-8'
-                    >
-                      Learn More
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* Logo Cloud */}
-          <div className='bg-gray-100 w-screen'>
-            <div className='max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8'>
-              <p className='text-center text-sm font-semibold uppercase text-gray-500 tracking-wide'>
-                Trusted partner of the Canadian Red Cross
-              </p>
-              <div className='mt-16 flex justify-center'>
-                <img
-                  src='https://seeklogo.com/images/C/Canadian_Red_Cross-logo-A82C55883F-seeklogo.com.png'
-                  alt='Red Cross'
-                />
-              </div>
-            </div>
-          </div>
-          {/* Gradient Feature Section */}
-          <div className='bg-gradient-to-r from-blue-600 to-blue-400'>
-            <div className='max-w-4xl mx-auto px-4 py-16 sm:px-6 sm:pt-20 sm:pb-24 lg:max-w-7xl lg:pt-24 lg:px-8 flex flex-col items-center'>
-              <h2 className='text-3xl font-extrabold text-white tracking-tight'>
-                Popular Courses
-              </h2>
-              <p className='mt-4 max-w-3xl text-lg text-purple-200 text-center'>
-                Browse our most highly recommended courses
-              </p>
-              <div className='mt-12 grid grid-cols-1 gap-x-6 gap-y-12 sm:grid-cols-2 lg:mt-16 lg:grid-cols-4 lg:gap-x-8 lg:gap-y-16'>
-                {features.map((feature) => (
-                  <div key={feature.name}>
-                    <div>
-                      <span className='flex items-center justify-center h-12 w-12 rounded-md bg-white bg-opacity-10'>
-                        <feature.icon
-                          className='h-6 w-6 text-white'
-                          aria-hidden='true'
-                        />
-                      </span>
-                    </div>
-                    <div className='mt-6'>
-                      <h3 className='text-lg font-medium text-white'>
-                        {feature.name}
-                      </h3>
-                      <p className='mt-2 text-base text-purple-200'>
-                        {feature.description}
-                      </p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
+    <div className='bg-white'>
+      {/* Hero section */}
+      <div className='relative bg-gray-900 shadow-lg shadow-slate-500'>
+        {/* Decorative image and overlay */}
+        <div aria-hidden='true' className='absolute inset-0 overflow-hidden'>
+          <img
+            src='/images/group-training-picture.jpg'
+            alt=''
+            className='w-full h-full object-center object-cover blur-sm'
+          />
+        </div>
+        <div
+          aria-hidden='true'
+          className='absolute inset-0 bg-blue-900 opacity-40'
+        />
 
-          {/* Stats section */}
-          <div className='relative bg-gray-900'>
-            <div className='h-80 absolute inset-x-0 bottom-0 xl:top-0 xl:h-full'>
-              <div className='h-full w-full xl:grid xl:grid-cols-2'>
-                <div className='h-full xl:relative xl:col-start-2'>
-                  <img
-                    className='h-full w-full object-cover opacity-25 xl:absolute xl:inset-0'
-                    src='https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100'
-                    alt='People working on laptops'
-                  />
-                  <div
-                    aria-hidden='true'
-                    className='absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-gray-900 xl:inset-y-0 xl:left-0 xl:h-full xl:w-32 xl:bg-gradient-to-r'
-                  />
-                </div>
-              </div>
+        <div className='relative max-w-3xl mx-auto py-32 px-6 flex flex-col items-center text-center sm:py-64 lg:px-0'>
+          <img
+            src='/images/logo-no-text.png'
+            alt=''
+            className='w-44 mb-24 h-auto object-center'
+          />
+          <h1 className='text-4xl font-thick font-bold tracking-tight text-white lg:text-6xl'>
+            PMD First Aid & CPR
+          </h1>
+          <p className='mt-4 text-3xl text-white font-header font-light'>
+            Bilingual — Accessible — Equitable
+          </p>
+          <div className='mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8'>
+            <div className='rounded-md shadow'>
+              <a
+                href='/courses'
+                className='w-full flex items-center justify-center px-8 py-3 border border-transparent font-header text-lg font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 md:py-4 md:text-xl md:px-10'
+              >
+                Browse courses
+              </a>
             </div>
-            <div className='max-w-4xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8 xl:grid xl:grid-cols-2 xl:grid-flow-col-dense xl:gap-x-8'>
-              <div className='relative pt-12 pb-64 sm:pt-24 sm:pb-64 xl:col-start-1 xl:pb-24'>
-                <p className='mt-3 text-3xl font-extrabold text-white'>
-                  How we set you on the path to success
-                </p>
-                <p className='mt-5 text-lg text-gray-300'>
-                  Rhoncus sagittis risus arcu erat lectus bibendum. Ut in
-                  adipiscing quis in viverra tristique sem. Ornare feugiat
-                  viverra eleifend fusce orci in quis amet. Sit in et vitae
-                  tortor, massa. Dapibus laoreet amet lacus nibh integer quis.
-                  Eu vulputate diam sit tellus quis at.
-                </p>
-                <div className='mt-12 grid grid-cols-1 gap-y-12 gap-x-6 sm:grid-cols-2'>
-                  {metrics.map((item) => (
-                    <p key={item.id}>
-                      <span className='block text-2xl font-bold text-white'>
-                        {item.stat}
-                      </span>
-                      <span className='mt-1 block text-base text-gray-300'>
-                        <span className='font-medium text-white'>
-                          {item.emphasis}
-                        </span>{' '}
-                        {item.rest}
-                      </span>
-                    </p>
-                  ))}
+            <div className='mt-3 rounded-md shadow sm:mt-0 sm:ml-3'>
+              <a
+                href='/contact-us'
+                className='w-full flex items-center justify-center px-8 py-3 border border-transparent font-header text-base font-medium rounded-md text-blue-500 bg-white hover:bg-blue-100 md:py-4 md:text-xl md:px-10'
+              >
+                Book now
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <main>
+        {/* Collection section */}
+        <section
+          aria-labelledby='collection-heading'
+          className='bg-gradient-to-b from-blue-200 to-white mx-auto pt-24 px-4 sm:pt-32 sm:px-6 lg:px-8'
+        >
+          <h2
+            id='collection-heading'
+            className='text-2xl font-extrabold tracking-tight text-gray-900'
+          >
+            Popular Courses
+          </h2>
+          <p className='mt-4 text-base text-gray-500'>
+            Our clients spoke and we listened. Here are their favourite courses
+            of 2022.
+          </p>
+
+          <div className='mt-10 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-4 lg:gap-x-8'>
+            {courses.map((collection) => (
+              <a
+                key={collection.name}
+                href={collection.href}
+                className='group block'
+              >
+                <div
+                  aria-hidden='true'
+                  className='aspect-w-3 aspect-h-2 rounded-lg overflow-hidden group-hover:opacity-75 lg:aspect-w-5 lg:aspect-h-6'
+                >
+                  <img
+                    src={collection.imageSrc}
+                    alt={collection.imageAlt}
+                    className='w-full h-full object-center object-cover'
+                  />
                 </div>
+                <h3 className='mt-4 text-base font-semibold text-gray-900'>
+                  {collection.name}
+                </h3>
+                <p className='mt-2 text-sm text-gray-500'>
+                  {collection.description}
+                </p>
+              </a>
+            ))}
+          </div>
+        </section>
+
+        {/* Featured section */}
+        <section
+          aria-labelledby='comfort-heading'
+          className=' mx-auto pt-24 pb-10 px-4  sm:px-6 lg:px-8'
+        >
+          <div className='relative rounded-lg overflow-hidden'>
+            <div className='absolute inset-0'>
+              <img
+                src='https://tailwindui.com/img/ecommerce-images/home-page-01-feature-section-02.jpg'
+                alt=''
+                className='w-full h-full object-center object-cover'
+              />
+            </div>
+            <div className='relative bg-gray-900 bg-opacity-75 py-32 px-6 sm:py-40 sm:px-12 lg:px-16'>
+              <div className='relative max-w-3xl mx-auto flex flex-col items-center text-center'>
+                <h2
+                  id='comfort-heading'
+                  className='text-3xl font-extrabold tracking-tight text-white sm:text-4xl'
+                >
+                  Know what you're looking for?
+                </h2>
+                <p className='mt-3 text-xl text-white'>Book with us today!</p>
+                <a
+                  href='/contact-us'
+                  className='mt-8 w-full block bg-white border border-transparent rounded-md py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto'
+                >
+                  Contact for booking
+                </a>
               </div>
             </div>
           </div>
-        </main>
-      </div>
-    </>
+        </section>
+
+        <section className='bg-gradient-to-t from-blue-200 to-white w-full h-fit pt-10'>
+          <div className='relative'>
+            <div
+              className='absolute inset-0 flex items-center'
+              aria-hidden='true'
+            >
+              <div className='w-full border-t border-gray-300' />
+            </div>
+            <div className='relative flex justify-center'>
+              <span className='px-3 bg-white font-light text-3xl font-header text-gray-700'>
+                Authorizations
+              </span>
+            </div>
+          </div>
+          <p className='max-w-xl mx-auto text-center mt-10 mb-14'>
+            PMD First Aid & CPR is a proud and authorized First Aid/CPR training
+            partner of the Canadian Red Cross. Through this partnership, PMD is
+            fully recognized by the WSIB under the red cross.
+          </p>
+          <div className='max-w-7xl mx-auto mt-6 grid grid-cols-2 md:grid-cols-3 lg:mt-8'>
+            <div className='col-span-1 flex justify-center items-center py-8 px-8 border-r-[1px] border-slate-300'>
+              <img
+                className='w-32 h-fit'
+                src='/images/logo-small-text.png'
+                alt='PMD Logo'
+              />
+            </div>
+            <div className='col-span-1 flex justify-center items-center py-8 px-8'>
+              <img
+                className='w-44 h-fit'
+                src='/images/red-cross-picture.png'
+                alt='Red Cross Logo'
+              />
+            </div>
+            <div className='col-span-1 flex justify-center items-center py-8 px-8 border-l-[1px] border-slate-300'>
+              <img
+                className='w-64'
+                src='/images/wsib-picture.png'
+                alt='WSIB Logo'
+              />
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
   );
 };
 export default Landing;
