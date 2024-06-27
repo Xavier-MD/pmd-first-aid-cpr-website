3f052ccde440c2fa859b01e10dda6425ece55dc1
Xavier-MD <xavier.pmd@outlook.com>
2024-06-27 15:52:53 -0400

    Implemented glass navigation bar changes, bound to all views.

--- src/components/Navigation/NavigationBarRemake.jsx
+++ src/components/Navigation/NavigationBarRemake.jsx
@@ -11,7 +11,7 @@ import TabButton from './TabButton';
 import LanguagesDropdown from './LanguagesDropdown';
 import MobileDropdown from './MobileDropdown';
 
-const NavigationBarRemake = function() {
+const NavigationBarRemake = function () {
   const { t } = useTranslation();
 
   const home_tab = t('home_tab');
@@ -30,42 +30,37 @@ const NavigationBarRemake = function() {
 
   const [open, setOpen] = useState(false);
 
-    return (
-      <Disclosure as='nav' className='bg-white shadow z-10 w-full fixed'>
-        {
-          <>
-            <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-              <div className='flex h-16 justify-between'>
-                <div className='flex items-center md:hidden'>
-                  <MobileDropdown open={ open } setOpen={ setOpen } />
-                  <LanguagesDropdown />
-                </div>
-                <div className='flex'>
-                  <div className='hidden md:flex justify-center items-center'>
-                    <div className='p-[0.80vh] bg-transparent'>
-                      <HomeButton />
-                    </div>
-                  </div>
-                  <div className='hidden md:ml-6 md:flex md:space-x-8'>
-                    {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
-                    <Tab tabInfo={tabsInfo.homepage} moveLeft='0' />
-                    <Tab tabInfo={tabsInfo.aboutUs} moveLeft='0' />
-                    <Tab tabInfo={tabsInfo.courses} moveLeft='0' />
-                    <Tab tabInfo={tabsInfo.testimonials} moveLeft='0' />
-                  </div>
-                </div>
-                <div className='flex items-center'>
-                  <div className='max-w-xl hidden lg:flex flex-1 justify-around items-center'>
-                    <LanguagesDropdown />
-                    <TabButton tabInfo={tabsInfo.contactUs} />
-                  </div>
-                </div>
-              </div>
+  return (
+    <Disclosure as='nav' className={classNames('/' === window.location.pathname ? 'bg-opacity-50' : 'bg-opacity-30', 'bg-blue-100 z-10 w-full fixed backdrop-blur-lg')}>
+      <div className='flex justify-between mx-auto max-w-screen-xl px-[1.5rem]'>
+        <div className='flex items-center md:hidden'>
+          <MobileDropdown open={open} setOpen={setOpen} />
+          <LanguagesDropdown />
+        </div>
+        <div className='flex justify-around'>
+          <div className='hidden md:flex justify-center items-center'>
+            <div className='p-[0.80vh] bg-transparent'>
+              <HomeButton />
             </div>
-          </>
-        }
-      </Disclosure>
-    );
- }
+          </div>
+          <div className='hidden md:ml-6 md:flex md:space-x-8'>
+            {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
+            <Tab tabInfo={tabsInfo.homepage} moveLeft='0' />
+            <Tab tabInfo={tabsInfo.aboutUs} moveLeft='0' />
+            <Tab tabInfo={tabsInfo.courses} moveLeft='0' />
+            <Tab tabInfo={tabsInfo.testimonials} moveLeft='0' />
+          </div>
+        </div>
+        <div className='flex items-center'>
+          <div className='max-w-xl hidden lg:flex flex-1 justify-around items-center'>
+            <LanguagesDropdown />
+            <TabButton tabInfo={tabsInfo.contactUs} />
+          </div>
+        </div>
+      </div>
+      <div className='w-[75vw] h-[0.05rem] bg-gradient-to-r from-transparent via-slate-400 to-transparent mx-auto' />
+    </Disclosure>
+  );
+};
 
 export default NavigationBarRemake;
