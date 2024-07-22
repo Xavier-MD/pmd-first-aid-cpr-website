f05902a8d4fe112444aaa911fbb236b62667f02a
Xavier-MD <xavier.pmd@outlook.com>
2024-07-21 20:34:59 -0400

    Revamped contact page UI design and sizing.

---
+++ src/components/Contact/ContactForm.jsx
@@ -0,0 +1,157 @@
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+import { useTranslation } from 'react-i18next';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
+const ContactForm = function () {
+  /* Translation Text Variables */
+  const { t } = useTranslation();
+  const contact_us_input_1 = t('contact_us_input_1');
+  const contact_us_input_2 = t('contact_us_input_2');
+  const contact_us_input_3 = t('contact_us_input_3');
+  const contact_us_input_4 = t('contact_us_input_4');
+  const contact_us_input_5 = t('contact_us_input_5');
+  const contact_us_input_6 = t('contact_us_input_6');
+  const contact_us_button_1 = t('contact_us_button_1');
+
+  /* emailJS Integration */
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_CONTACT_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_USER_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  /* Input Field Formatter */
+  const contactFormInput = function ({ label, id, type, autoComplete, input, placeholder, required }) {
+    let inputField;
+    const inputFieldAttributes =
+      'w-full bg-blue-100 border-0 rounded-md text-sm text-slate-700 focus:ring-[0.1rem] focus:ring-blue-500 placeholder:text-blue-300';
+    switch (input) {
+      case 'inputField':
+        inputField = (
+          <input
+            required={required}
+            id={id}
+            name={id}
+            type={type}
+            autoComplete={autoComplete}
+            placeholder={placeholder}
+            className={inputFieldAttributes}
+          />
+        );
+        break;
+      case 'textArea':
+        inputField = <textarea required id={id} name={id} type={type} rows={3} className={inputFieldAttributes} />;
+        break;
+      default:
+        inputField = <div />;
+    }
+    return (
+      <>
+        <label htmlFor={id} className=' ml-[0.1rem] text-[0.75rem] font-font4 text-slate-500'>
+          {label}
+        </label>
+        {inputField}
+      </>
+    );
+  };
+
+  return (
+    <>
+      {/* Contact Form */}
+      <div className='z-10 mx-[2rem] px-[2.5rem] pt-[2rem] pb-[1.5rem] bg-white rounded-md shadow-2xl shadow-blue-800'>
+        <form ref={form} onSubmit={sendEmail} className='w-full grid grid-cols-10 gap-x-[1rem] gap-y-[0.5rem]'>
+          {/* Contact Form Fields */}
+          <div className='col-span-10 lg:col-span-5'>
+            {contactFormInput({
+              label: contact_us_input_1,
+              id: 'first-name',
+              type: 'text',
+              autoComplete: 'given-name',
+              placeholder: 'Jean-Luc',
+              input: 'inputField',
+              required: true
+            })}
+          </div>
+          <div className='col-span-10 lg:col-span-5'>
+            {contactFormInput({
+              label: contact_us_input_2,
+              id: 'last-name',
+              type: 'text',
+              autoComplete: 'family-name',
+              placeholder: 'Maka',
+              input: 'inputField',
+              required: true
+            })}
+          </div>
+          <div className='col-span-10 lg:col-span-5'>
+            {contactFormInput({
+              label: contact_us_input_4,
+              id: 'email',
+              type: 'email',
+              autoComplete: 'email',
+              placeholder: 'jm123@gmail.com',
+              input: 'inputField',
+              required: true
+            })}
+          </div>
+          <div className='col-span-10 lg:col-span-5'>
+            {contactFormInput({
+              label: contact_us_input_5,
+              id: 'phone',
+              type: 'text',
+              autoComplete: 'tel',
+              placeholder: '555-987-6543',
+              input: 'inputField',
+              required: false
+            })}
+          </div>
+          <div className='col-span-10'>
+            {contactFormInput({
+              label: contact_us_input_3,
+              id: 'subject',
+              type: 'text',
+              autoComplete: 'subject',
+              input: 'inputField',
+              required: true
+            })}
+          </div>
+          <div className='col-span-10'>
+            {contactFormInput({
+              label: contact_us_input_6,
+              id: 'message',
+              type: 'text',
+              input: 'textArea',
+              required: true
+            })}
+          </div>
+          {/* Submit Button */}
+          <button
+            type='submit'
+            className='w-fit py-[0.3rem] pl-[0.1rem] pr-[0.5rem] flex items-center bg-transparent text-base font-font4 font-semibold text-blue-500 hover:text-blue-400 focus:text-blue-600'
+          >
+            {contact_us_button_1} <FontAwesomeIcon icon={faArrowRight} className='ml-[0.5rem]' />
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default ContactForm;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,7 @@
-import { useRef } from 'react';
-import emailjs from '@emailjs/browser';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
-import { faComments } from '@fortawesome/free-regular-svg-icons';
 import { useTranslation } from 'react-i18next';
+import ContactForm from '../components/Contact/ContactForm';
 
 const Contact = function () {
   const { t } = useTranslation();
@@ -12,226 +9,42 @@ const Contact = function () {
   const contact_us_title_1 = t('contact_us_title_1');
   const contact_us_title_2 = t('contact_us_title_2');
   const contact_us_text_1 = t('contact_us_text_1');
-  const contact_us_input_1 = t('contact_us_input_1');
-  const contact_us_input_2 = t('contact_us_input_2');
-  const contact_us_input_3 = t('contact_us_input_3');
-  const contact_us_input_4 = t('contact_us_input_4');
-  const contact_us_input_5 = t('contact_us_input_5');
-  const contact_us_input_6 = t('contact_us_input_6');
-  const contact_us_button_1 = t('contact_us_button_1');
 
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-    emailjs
-      .sendForm(
-        process.env.REACT_APP_SERVICE_ID,
-        process.env.REACT_APP_CONTACT_TEMPLATE_ID,
-        form.current,
-        process.env.REACT_APP_USER_ID
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+  const ctaSection = function () {
+    return (
+      <div className='lg:pt-[0.5rem] flex flex-col'>
+        <h2 className='text-blue-500 text-xl font-font1'>{contact_us_title_2}</h2>
+        <span className='mb-[0.1rem] ml-[0rem] lg:ml-[0.8rem] flex justify-center lg:justify-start items-center text-blue-500 font-font1'>
+          <FontAwesomeIcon icon={faEnvelope} className='mr-[0.8rem]' />
+          paul@pmdcpr.com
+        </span>
+        <span className='mb-[2rem] ml-[0rem] lg:ml-[0.8rem] flex justify-center lg:justify-start items-center text-blue-500 font-font1'>
+          <FontAwesomeIcon icon={faPhone} className='mr-[0.8rem]' />
+          (416)-578-4367
+        </span>
+      </div>
+    );
   };
 
   return (
-    <div className='bg-white mt-24'>
-      <main>
-        {/* Contact Section */}
-        <div className='relative bg-gradient-to-t from-blue-300 to-white'>
-          <div className='lg:absolute lg:inset-0'>
-            <div className='lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2'>
-              <img
-                className='-mt-20 sm:-mt-40 md:-mt-60 lg:-mt-0 h-full w-full object-cover lg:absolute lg:h-full'
-                src='/images/contact-us-picture.jpg'
-                alt=''
-              />
-            </div>
-          </div>
-          <div className='lg:relative lg:w-1/2 py-16 px-4 sm:py-24 sm:px-6 lg:px-8 lg:py-20'>
-            <div className='w-full flex flex-col items-center p-8 2xl:px-20 drop-shadow-md'>
-              <h2 className='text-3xl text-center font-header font-light sm:text-5xl'>
-                <FontAwesomeIcon
-                  icon={faComments}
-                  className='mr-10 text-blue-500'
-                />
-                {contact_us_title_1}
-                <FontAwesomeIcon
-                  icon={faComments}
-                  className='ml-10 opacity-0'
-                />
-              </h2>
-              <p className='mt-4 text-lg text-center font-body text-gray-500 sm:mt-5 sm:mb-10'>
-                {contact_us_text_1}
-              </p>
-              <form
-                ref={form}
-                onSubmit={sendEmail}
-                className='w-full font-body grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8'
-              >
-                <div>
-                  <label
-                    htmlFor='first-name'
-                    className='block text-sm font-medium text-gray-700'
-                  >
-                    {contact_us_input_1}
-                  </label>
-                  <div className='mt-1'>
-                    <input
-                      required
-                      type='text'
-                      name='first-name'
-                      id='first-name'
-                      autoComplete='given-name'
-                      className='py-3 px-4 block w-full  focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md'
-                    />
-                  </div>
-                </div>
-                <div>
-                  <label
-                    htmlFor='last-name'
-                    className='block text-sm font-medium text-gray-700'
-                  >
-                    {contact_us_input_2}
-                  </label>
-                  <div className='mt-1'>
-                    <input
-                      required
-                      type='text'
-                      name='last-name'
-                      id='last-name'
-                      autoComplete='family-name'
-                      className='py-3 px-4 block w-full  focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md'
-                    />
-                  </div>
-                </div>
-                <div className='sm:col-span-2'>
-                  <label
-                    htmlFor='subject'
-                    className='block text-sm font-medium text-gray-700'
-                  >
-                    {contact_us_input_3}
-                  </label>
-                  <div className='mt-1'>
-                    <input
-                      required
-                      type='text'
-                      name='subject'
-                      id='subject'
-                      autoComplete='subject'
-                      className='py-3 px-4 block w-full  focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md'
-                    />
-                  </div>
-                </div>
-                <div className='sm:col-span-2'>
-                  <label
-                    htmlFor='email'
-                    className='block text-sm font-medium text-gray-700'
-                  >
-                    {contact_us_input_4}
-                  </label>
-                  <div className='mt-1'>
-                    <input
-                      required
-                      id='email'
-                      name='email'
-                      type='email'
-                      autoComplete='email'
-                      className='py-3 px-4 block w-full  focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md'
-                    />
-                  </div>
-                </div>
-                <div className='sm:col-span-2'>
-                  <div className='flex'>
-                    <label
-                      htmlFor='phone'
-                      className='block text-sm font-medium text-gray-700'
-                    >
-                      {contact_us_input_5}
-                    </label>
-                  </div>
-                  <div className='mt-1 relative rounded-md '>
-                    <div className='absolute inset-y-0 left-0 flex items-center'>
-                      <label htmlFor='country' className='sr-only'>
-                        Country
-                      </label>
-                      <select
-                        id='country'
-                        name='country'
-                        className='h-full py-0 pl-4 pr-8 border-transparent bg-transparent text-gray-500 focus:ring-blue-500 focus:border-blue-500 rounded-md'
-                      >
-                        <option>CA</option>
-                        <option>US</option>
-                        <option>EU</option>
-                      </select>
-                    </div>
-                    <input
-                      type='text'
-                      name='phone'
-                      id='phone'
-                      autoComplete='tel'
-                      className='py-3 px-4 block w-full pl-20 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md'
-                      placeholder='+1 (555) 987-6543'
-                    />
-                  </div>
-                </div>
-                <div className='sm:col-span-2'>
-                  <label
-                    htmlFor='message'
-                    className='block text-sm font-medium text-gray-700'
-                  >
-                    {contact_us_input_6}
-                  </label>
-                  <div className='mt-1'>
-                    <textarea
-                      required
-                      type='text'
-                      id='message'
-                      name='message'
-                      rows={4}
-                      className='py-3 px-4 block w-full  focus:ring-blue-500 focus:border-blue-500 border border-gray-300 rounded-md'
-                      defaultValue={''}
-                    />
-                  </div>
-                </div>
-                <div className='sm:col-span-2'>
-                  <button
-                    type='submit'
-                    className='w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md  text-base font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500'
-                  >
-                    {contact_us_button_1}
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
+    <>
+      <div className='grid grid-cols-1 lg:grid-cols-2'>
+        {/* Left Side Content */}
+        <div className='h-full mx-[2rem] flex flex-col justify-center items-center lg:items-start space-y-[1rem] text-center lg:text-left'>
+          {/* Header Text */}
+          <h2 className='text-4xl text-slate-600 font-thick font-bold'>{contact_us_title_1}</h2>
+          <p className='max-w-lg pb-[2rem] lg:pb-[0.5rem] font-sans text-gray-500'>{contact_us_text_1}</p>
+          {/* Divider */}
+          <div className='w-full h-[0.2rem] hidden lg:block rounded-lg bg-gradient-to-r from-blue-400 to-50%' />
+          <div className='hidden lg:flex'>{ctaSection()}</div>
         </div>
-
-        {/* CTA Section */}
-        <div className='bg-blue-500'>
-          <div className='flex flex-col max-w-md mx-auto text-center py-16 px-4 sm:max-w-2xl sm:py-24 sm:px-6 lg:px-8 space-y-2'>
-            <h2 className='text-blue-50 text-3xl font-header font-light sm:text-4xl mb-4'>
-              {contact_us_title_2}
-            </h2>
-            <span className='text-blue-50 text-lg font-body font-light sm:text-xl'>
-              <FontAwesomeIcon icon={faEnvelope} className='mr-4' />
-              paul@pmdcpr.com
-            </span>
-            <span className='text-blue-50 text-lg font-body font-light sm:text-xl'>
-              <FontAwesomeIcon icon={faPhone} className='mr-4' />
-              (416)-578-4367
-            </span>
-          </div>
+        {/* Right Side Content */}
+        <div className='w-full h-full pl-[1rem] flex items-center'>
+          <ContactForm />
         </div>
-      </main>
-    </div>
+        <div className='mt-[4rem] mb-[-1rem] flex lg:hidden justify-center'>{ctaSection()}</div>
+      </div>
+    </>
   );
 };
 
