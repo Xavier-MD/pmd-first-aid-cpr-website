3d0fbf79363be66de98eb7e367486aeedc79f823
Xavier-MD <xavier.pmd@outlook.com>
2024-09-11 19:21:27 -0400

    Added an animated text gradient to the navbar.

--- src/components/Navigation/NavigationBar.jsx
+++ src/components/Navigation/NavigationBar.jsx
@@ -29,16 +29,24 @@ const NavigationBar = function () {
     <>
       {/* Navbar */}
       <Disclosure as='nav'>
-        <div className='w-full max-w-7xl z-50 mx-auto px-[3rem] grid grid-cols-3 md:grid-cols-7 bg-transparent'>
+        <div className='w-full max-w-7xl z-50 mx-auto px-[3rem] grid grid-cols-3 md:grid-cols-7 lg:grid-cols-9 bg-transparent'>
           {/* Mobile Menu */}
           <div className='mt-[1.5rem] ml-[4vw] flex flex-1 md:hidden justify-start'>
             <MobileDropdown open={open} setOpen={setOpen} />
           </div>
-          {/* Desktop Tabs */}
-          <a href='/' className='flex justify-center md:justify-start'>
-            <img className='w-auto h-[4rem] md:h-[2.5rem] mt-[1rem]' src='/images/logo-no-text.png' alt='PMD Logo' />
+          {/* PMD Logo */}
+          <a href='/' className='flex justify-center items-center md:justify-start lg:col-span-2'>
+            <img
+              className='w-auto h-[4rem] md:h-[2.5rem] mt-[1rem] mr-[0.7rem]'
+              src='/images/logo-no-text.png'
+              alt='PMD Logo'
+            />
+            <div className='mt-[1.5rem] font-font4 font-medium tracking-tight bg-gradient-to-r from-green-400 via-slate-700 to-blue-400 text-transparent bg-clip-text bg-300% animate-gradient'>
+              <p className='invisible lg:visible'>PMD First Aid & CPR</p>
+            </div>
           </a>
-          <div className='mt-[1.5rem] hidden md:flex justify-center items-center space-x-[3rem] col-start-2 col-span-5 text-nowrap'>
+          {/* Desktop Tabs */}
+          <div className='mt-[1.5rem] hidden md:flex justify-center items-center space-x-[3rem] col-start-2 lg:col-start-3 col-span-5 text-nowrap'>
             <DesktopTab tabInfo={tabsInfo.homepage} moveLeft='0' />
             <DesktopTab tabInfo={tabsInfo.aboutUs} moveLeft='0' />
             <DesktopTab tabInfo={tabsInfo.courses} moveLeft='0' />
@@ -46,7 +54,7 @@ const NavigationBar = function () {
             <DesktopTab tabInfo={tabsInfo.contactUs} moveLeft='0' />
           </div>
           {/* Mobile Language Toggle Dropdown */}
-          <div className='mt-[1.5rem] mr-[4vw] md:mr-[0rem] flex justify-end items-center md:col-start-7'>
+          <div className='mt-[1.5rem] mr-[4vw] md:mr-[0rem] flex justify-end items-center md:col-start-7 lg:col-start-9'>
             <LanguagesDropdown />
           </div>
         </div>
