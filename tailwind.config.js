bb566f0d201c839dbd743ca7bf9deb6ee3b270a6
Xavier-MD <xavier.pmd@outlook.com>
2024-09-28 16:51:19 -0400

    Added css for home button animation.

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,25 +14,25 @@ module.exports = {
       font6: ['Slabo 27px']
     },
     extend: {
+      animation: {
+        gradient: 'animatedgradient 12s ease infinite alternate'
+      },
       keyframes: {
         animatedgradient: {
           '0%': { backgroundPosition: '0% 50%' },
           '50%': { backgroundPosition: '100% 50%' },
-          '100%': { backgroundPosition: '0% 50%' },
-        },
+          '100%': { backgroundPosition: '0% 50%' }
+        }
       },
       backgroundSize: {
-        '300%': '300%',
-      },
-      animation: {
-        gradient: 'animatedgradient 12s ease infinite alternate',
-      },
-    },
+        '300%': '300%'
+      }
+    }
   },
   plugins: [
-    require('@tailwindcss/typography'), 
+    require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
-    require('tailwind-gradient-mask-image'),
+    require('tailwind-gradient-mask-image')
   ]
 };
