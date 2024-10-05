4084f0cb65c05cbac146a494506a26782c968a2d
Xavier-MD <xavier.pmd@outlook.com>
2024-10-04 20:19:02 -0400

    Changed a few design aspects on the landing page.

--- src/components/Landing/LandingGrid.jsx
+++ src/components/Landing/LandingGrid.jsx
@@ -1,19 +1,19 @@
 export const LandingGrid = () => {
   return (
-    <div className='w-full h-full flex justify-between'>
+    <div className='w-full h-full flex justify-between text-center font-font4'>
       <div className='h-full w-[15rem] flex flex-col'>
         <img
           className='m-[0.5rem] rounded-2xl shadow-xl'
           src='/images/cpr_landing.jpeg'
           alt='woman performing cpr on mannequin'
         />
-        <div className='h-full m-[0.5rem] rounded-xl bg-white'>
-          <p>Learn more about what makes us special</p>
+        <div className='h-full m-[0.5rem] flex items-center rounded-xl bg-white shadow-lg'>
+          <p>Learn more about what makes us special!</p>
         </div>
       </div>
       <div className='h-full w-[12rem] flex flex-col'>
-        <div className='h-full m-[0.5rem] rounded-xl bg-white'>
-          <p>Questions? Message our trainers directly </p>
+        <div className='h-full m-[0.5rem] flex items-center rounded-xl bg-white shadow-lg'>
+          <p>Questions? Message our team directly. </p>
         </div>
         <img
           className='m-[0.5rem] rounded-2xl shadow-xl'
@@ -27,8 +27,8 @@ export const LandingGrid = () => {
           src='/images/cpr_form_landing.jpg'
           alt='group of students demonstrating proper cpr form'
         />
-        <div className='h-full m-[0.5rem] rounded-xl bg-white'>
-          <p>Learn more about what makes us special</p>
+        <div className='h-full m-[0.5rem] flex items-center rounded-xl bg-white shadow-lg'>
+          <p>All services available in English and French.</p>
         </div>
       </div>
     </div>
