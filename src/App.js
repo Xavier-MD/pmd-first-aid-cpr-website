7362666f5c9ec26b864996d6f56ed4f56dc0a5cd
Xavier-MD <xavier.pmd@outlook.com>
2024-10-25 18:45:39 -0400

    Removed Navbar and Footer components from App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 
-import NavigationBar from './components/Navigation/NavigationBar';
-import Footer from './components/Navigation/Footer';
 import Landing from './pages/Landing';
 import About from './pages/About';
 import CourseSelect from './pages/CourseSelect';
@@ -15,21 +13,17 @@ function App() {
   return (
     <BrowserRouter>
       <div className='w-screen min-h-screen flex flex-col overflow-x-hidden'>
-        <NavigationBar />
-        <div className='px-[3.2rem] py-[1.7rem] flex grow justify-center items-center bg-gradient-to-b from-white via-blue-100 to-blue-300'>
-          <div className='w-full max-w-7xl mx-auto'>
-            <Routes>
-              <Route path='/' element={<Landing />} />
-              <Route path='/about-us' element={<About />} />
-              <Route path='/courses' element={<CourseSelect />} />
-              <Route path='/courses/:id' element={<Course />} />
-              <Route path='/testimonials' element={<Testimonial />} />
-              <Route path='/contact' element={<Contact />} />
-              <Route path='*' element={<Error />} />
-            </Routes>
-          </div>
+        <div className='flex grow bg-gradient-to-b from-white via-blue-100 to-blue-300'>
+          <Routes>
+            <Route path='/' element={<Landing />} />
+            <Route path='/about-us' element={<About />} />
+            <Route path='/courses' element={<CourseSelect />} />
+            <Route path='/courses/:id' element={<Course />} />
+            <Route path='/testimonials' element={<Testimonial />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='*' element={<Error />} />
+          </Routes>
         </div>
-        <Footer />
       </div>
       <Toaster />
     </BrowserRouter>
