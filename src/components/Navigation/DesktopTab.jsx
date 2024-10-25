c36f6ec7161754cacbbc89bbf2dec3cdc104b546
Xavier-MD <xavier.pmd@outlook.com>
2024-10-25 18:46:23 -0400

    Changed navbar tab text font size.

--- src/components/Navigation/DesktopTab.jsx
+++ src/components/Navigation/DesktopTab.jsx
@@ -5,20 +5,13 @@ const DesktopTab = function ({ tabInfo, moveLeft }) {
     <a href={tabInfo.href} className='group inline-flex flex-col justify-center items-center focus:outline-none'>
       <h2
         className={classNames(
-          window.location.pathname === tabInfo.href ? 'text-blue-400' : 'text-slate-500 group-hover:text-blue-400',
-          'text-[0.95rem] font-font4 transition ease-in-out group-hover:-translate-y-[0.05rem] group-hover:scale-[110%] duration-200'
+          window.location.pathname === tabInfo.href ? 'text-blue-600' : 'text-slate-600 group-hover:text-blue-500',
+          'text-[0.90rem] font-lato transition ease-in-out group-hover:-translate-y-[0.05rem] group-hover:scale-[110%] duration-200'
         )}
       >
         {tabInfo.name}
       </h2>
-      <div
-        className={classNames(
-          window.location.pathname === tabInfo.href
-            ? ''
-            : 'transition ease-in-out duration-700 opacity-45 group-hover:opacity-80',
-          'bg-blue-300 w-full h-[0.09rem] rounded-lg'
-        )}
-      />
+      
     </a>
   );
 };
